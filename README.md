97678cf09b149ea50303b652f42f9e67028a111d
Jakka Prihatna <jprihatna@gmail.com>
2021-02-26 23:20:03 +0700

    feat: initial commit
    Signed-off-by: Jakka Prihatna <jprihatna@gmail.com>

---
+++ README.md
@@ -0,0 +1,30 @@
+<h1><p align="center" style="font-size:1.5rem;font-weight:normal"><img src="images/icon.png" alt="VS Code DarkLight" width="128" height=128" /><br /><strong>DarkLight</strong></p></h1>
+
+[![GitHub build](https://github.com/IronGeek/vscode-darklight/workflows/Build/badge.svg)](https://github.com/IronGeek/vscode-darklight/actions/workflows/build.yml)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/IronGeek/vscode-darklight?logo=github)](https://github.com/IronGeek/vscode-darklight/releases/latest)
+[![GitHub package.json version](https://img.shields.io/github/package-json/v/IronGeek/vscode-darklight?logo=github)](https://github.com/IronGeek/vscode-darklight)
+[![Visual Studio Marketplace Version](https://img.shields.io/visual-studio-marketplace/v/IronGeek.vscode-darklight?label=VS%20Marketplace&logo=visual-studio-code)](https://marketplace.visualstudio.com/items?itemName=IronGeek.vscode-darklight)
+[![Open VSX Version](https://img.shields.io/open-vsx/v/IronGeek/vscode-darklight)](https://open-vsx.org/extension/IronGeek/vscode-darklight)
+[![GitHub](https://img.shields.io/github/license/IronGeek/vscode-darklight)](LICENSE)
+
+Color mode switcher for Visual Studio Code
+
+
+## Features
+
+Adds action to easily switch between dark and light color mode:
+
+![feature](images/feature.gif)
+
+- From status bar
+
+  Click the `color-mode` button in VS Code status bar to toggle the dark or light color mode  
+
+- Using keyboard shortcuts
+
+  Use <kbd>CTRL</kbd>+<kbd>T</kbd>, <kbd>CTRL</kbd>+<kbd>K</kbd> to toggle the dark or light color mode
+
+
+## License
+
+This project is licensed under the terms of [The MIT license](LICENSE).
---
+++ src/extension.ts
@@ -0,0 +1,128 @@
+import {
+  window, workspace, commands,
+  OutputChannel, ExtensionContext,
+  StatusBarItem, StatusBarAlignment,
+  WorkspaceConfiguration, ConfigurationTarget
+} from 'vscode';
+
+const SETTING_KEY_WORKBENCH = 'workbench';
+const SETTING_KEY_COLOR_THEME = 'colorTheme';
+const SETTING_KEY_LIGHT_THEME = 'preferredLightColorTheme';
+const SETTING_KEY_DARK_THEME = 'preferredDarkColorTheme';
+
+const COMMAND_TOGGLE_THEME = 'darkLight.toggleColorMode';
+
+let themeStatusBarItem: StatusBarItem;
+
+interface ILogger {
+  log(...msg: any[]): void;
+}
+
+class Logger {
+  constructor(readonly output: OutputChannel) {
+    this.output = output;
+  }
+
+  log(...messages: any[]): void {
+    const d = new Date();
+    const date = d.toISOString().split('T')[0];
+    const time = d.toTimeString().split(' ')[0];
+    const ms = (d.getMilliseconds() + '').padStart(3, '0');
+
+    const msg = messages.map((message: any) => {
+      if (typeof message === 'string') {
+        return message;
+      } else if (typeof message === 'object') {
+        return this.encode(message);
+      }
+      return message.toString();
+    }).join('\n');
+
+    this.output.appendLine(`[${date} ${time}.${ms}] ${msg}`);
+  }
+
+  encode(obj: any): string {
+    return JSON.stringify(obj, null, 2);
+  }
+
+  dispose(): void {
+    this.output.dispose();
+  }
+}
+
+class ColorMode {
+  constructor(private logger?: ILogger) {
+  }
+
+  private get configuration(): WorkspaceConfiguration {
+    return workspace.getConfiguration(SETTING_KEY_WORKBENCH);
+  }
+
+  get current(): WorkspaceConfiguration | undefined {
+    return this.configuration
+      .get(SETTING_KEY_COLOR_THEME);
+  }
+
+  get mode(): 'dark' | 'light' {
+    return this.current === this.light ? 'light' : 'dark';
+  }
+
+  get light(): WorkspaceConfiguration | undefined {
+    return this.configuration
+      .get(SETTING_KEY_LIGHT_THEME);
+  }
+
+  get dark(): WorkspaceConfiguration | undefined {
+    return this.configuration
+      .get(SETTING_KEY_DARK_THEME);
+  }
+
+  async toggle(): Promise<ColorMode> {
+    try {
+      const newTheme = (this.current === this.light) ? this.dark : this.light;
+      this.logger?.log(`switching color mode: (${this.current}) ➝ (${newTheme})`);
+
+      await this.configuration
+        .update(SETTING_KEY_COLOR_THEME, newTheme, ConfigurationTarget.Global);
+    } catch (err) {
+      this.logger?.log(err, this.configuration);
+    }
+
+    return this;
+  }
+}
+
+export function activate(context: ExtensionContext) {
+  const logger = new Logger(window.createOutputChannel('DarkLight'));
+  logger.log('activating extension');
+
+  const toggleColorMode = async (confirm?: boolean): Promise<void> => {
+    const colorMode = await new ColorMode(logger).toggle();
+    if (confirm) {
+      return;
+    }
+
+    if (await window.showInformationMessage(
+      `Switched to ${colorMode.mode} mode (${colorMode.current})`,
+      'Revert', 'Dismiss'
+    ) === 'Revert') {
+      toggleColorMode(true);
+    }
+  };
+
+  const switchThemeCommand = commands.registerCommand(COMMAND_TOGGLE_THEME, toggleColorMode);
+
+  themeStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 99);
+  themeStatusBarItem.command = COMMAND_TOGGLE_THEME;
+  themeStatusBarItem.text = '$(color-mode)';
+  themeStatusBarItem.tooltip = 'Toggle VS Code color mode';
+  themeStatusBarItem.show();
+
+  context.subscriptions.push(
+    logger,
+    switchThemeCommand,
+    themeStatusBarItem
+  );
+}
+
+export function deactivate() { }
