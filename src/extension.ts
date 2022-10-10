a38c6d53169f7c872f59131268b9eb0c91c99ccb
Jakka Prihatna <jprihatna@gmail.com>
2022-10-10 16:47:40 +0700

    feat: add support for high contrast mode
    Signed-off-by: Jakka Prihatna <jprihatna@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,7 @@
 import {
   window, workspace, commands,
   OutputChannel, ExtensionContext,
-  StatusBarItem, StatusBarAlignment,
-  WorkspaceConfiguration, ConfigurationTarget
+  StatusBarAlignment, WorkspaceConfiguration, ConfigurationTarget
 } from 'vscode';
 
 const SETTING_KEY_CONFIGURATION = 'darkLight';
@@ -10,18 +9,20 @@ const SETTING_KEY_WORKBENCH = 'workbench';
 const SETTING_KEY_COLOR_THEME = 'colorTheme';
 const SETTING_KEY_LIGHT_THEME = 'preferredLightColorTheme';
 const SETTING_KEY_DARK_THEME = 'preferredDarkColorTheme';
+const SETTING_KEY_HIGH_CONTRAST_LIGHT_THEME = 'preferredHighContrastLightColorTheme';
+const SETTING_KEY_HIGH_CONTRAST_DARK_THEME = 'preferredHighContrastColorTheme';
 
 const COMMAND_TOGGLE_THEME = 'darkLight.toggleColorMode';
-
-let themeStatusBarItem: StatusBarItem;
 
 interface ILogger {
   log(...msg: any[]): void;
 }
 
 class Logger {
-  constructor(readonly output: OutputChannel) {
-    this.output = output;
+  readonly #output: OutputChannel;
+
+  constructor(output: OutputChannel) {
+    this.#output = output;
   }
 
   log(...messages: any[]): void {
@@ -39,7 +40,7 @@ class Logger {
       return message.toString();
     }).join('\n');
 
-    this.output.appendLine(`[${date} ${time}.${ms}] ${msg}`);
+    this.#output.appendLine(`[${date} ${time}.${ms}] ${msg}`);
   }
 
   encode(obj: any): string {
@@ -47,89 +48,118 @@ class Logger {
   }
 
   dispose(): void {
-    this.output.dispose();
+    this.#output.dispose();
   }
 }
 
 class ColorMode {
-  constructor(private logger?: ILogger) {
+  #reversing: boolean = false;
+
+  readonly #logger?: ILogger;
+
+  constructor(logger?: ILogger) {
+    this.#logger = logger;
   }
 
-  private get configuration(): WorkspaceConfiguration {
+  get #configuration(): WorkspaceConfiguration {
     return workspace.getConfiguration(SETTING_KEY_CONFIGURATION);
   }
 
-  private get workbench(): WorkspaceConfiguration {
+  get #workbench(): WorkspaceConfiguration {
     return workspace.getConfiguration(SETTING_KEY_WORKBENCH);
   }
 
-  get current(): WorkspaceConfiguration | undefined {
-    return this.workbench
-      .get(SETTING_KEY_COLOR_THEME);
+  get reversing(): boolean {
+    return this.#reversing;
   }
 
-  get mode(): 'dark' | 'light' {
-    return this.current === this.light ? 'light' : 'dark';
+  get current(): string {
+    return this.#workbench.get(SETTING_KEY_COLOR_THEME) as string;
   }
 
-  get light(): WorkspaceConfiguration | undefined {
-    return this.workbench
-      .get(SETTING_KEY_LIGHT_THEME);
+  get isHighContrast(): boolean {
+    return this.current === this.#workbench.get(SETTING_KEY_HIGH_CONTRAST_LIGHT_THEME) ||
+      this.current === this.#workbench.get(SETTING_KEY_HIGH_CONTRAST_DARK_THEME);
   }
 
-  get dark(): WorkspaceConfiguration | undefined {
-    return this.workbench
-      .get(SETTING_KEY_DARK_THEME);
+  get isDark(): boolean {
+    return this.current === this.#workbench.get(SETTING_KEY_DARK_THEME) ||
+      this.current === this.#workbench.get(SETTING_KEY_HIGH_CONTRAST_DARK_THEME);
+  }
+
+  get light(): string | undefined {
+    return this.isHighContrast
+      ? this.#workbench.get(SETTING_KEY_HIGH_CONTRAST_LIGHT_THEME)
+      : this.#workbench.get(SETTING_KEY_LIGHT_THEME);
+  }
+
+  get dark(): string | undefined {
+    return this.isHighContrast
+      ? this.#workbench.get(SETTING_KEY_HIGH_CONTRAST_DARK_THEME)
+      : this.#workbench.get(SETTING_KEY_DARK_THEME);
   }
 
   get showNotification(): boolean {
-    return !!this.configuration
-      .get<boolean>('showNotification');
+    return !!this.#configuration.get<boolean>('showNotification');
   }
 
-  async toggle(): Promise<ColorMode> {
+  async revert(): Promise<void> {
+    if (this.#reversing === true) { return; }
+
+    this.#reversing = true;
+    await this.toggle();
+    this.#reversing = false;
+  }
+
+  async toggle(): Promise<void> {
+    const newTheme = this.isDark ? this.light : this.dark;
+    this.#logger?.log(`${this.#reversing ? 'reversing' : 'switching'} color theme: ${this.current} ➝ ${newTheme}`);
+
     try {
-      const newTheme = (this.current === this.light) ? this.dark : this.light;
-      this.logger?.log(`switching color mode: (${this.current}) ➝ (${newTheme})`);
-
-      await this.workbench
-        .update(SETTING_KEY_COLOR_THEME, newTheme, ConfigurationTarget.Global);
+      await this.#workbench.update(SETTING_KEY_COLOR_THEME, newTheme, ConfigurationTarget.Global);
     } catch (err) {
-      this.logger?.log(err, this.workbench);
+      this.#logger?.log(err, this.#workbench);
     }
-
-    return this;
   }
-}
 
-export function activate(context: ExtensionContext) {
-  const logger = new Logger(window.createOutputChannel('Dark Light'));
-  logger.log('activating extension');
-
-  const toggleColorMode = async (confirm?: boolean): Promise<void> => {
-    const colorMode = await new ColorMode(logger).toggle();
-    if (confirm || !colorMode.showNotification) {
-      return;
-    }
+  async confirm(): Promise<void> {
+    if (!this.showNotification) { return; }
 
     const action = await window.showInformationMessage(
-      `Switched to ${colorMode.mode} mode (${colorMode.current})`,
+      `Switched color mode to ${this.current}`,
       'Settings', 'Revert', 'Dismiss'
     );
 
     switch(action) {
       case 'Revert':
-        await toggleColorMode(true);
+        await this.revert();
         break;
       case 'Settings':
         await commands.executeCommand('workbench.action.openSettings', `@ext:irongeek.vscode-darklight`);
         break;
     }
-  };
+  }
+}
 
-  const switchThemeCommand = commands.registerCommand(COMMAND_TOGGLE_THEME, toggleColorMode);
+export function activate(context: ExtensionContext) {
+  const logger = new Logger(window.createOutputChannel('Dark Light'));
+  const colorMode = new ColorMode(logger);
 
-  themeStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 99);
+  logger.log('activating extension');
+  logger.log(`current color theme: ${colorMode.current}`);
+
+  const switchThemeCommand = commands.registerCommand(COMMAND_TOGGLE_THEME, () => colorMode.toggle());
+  const configurationChanged = workspace.onDidChangeConfiguration(async (e) => {
+    if (e.affectsConfiguration(`${SETTING_KEY_WORKBENCH}.${SETTING_KEY_COLOR_THEME}`)) {
+      logger.log(`current color theme: ${colorMode.current}`);
+
+      if (!colorMode.reversing) {
+        await colorMode.confirm();
+      }
+    }
+  });
+
+  const themeStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 99);
   themeStatusBarItem.command = COMMAND_TOGGLE_THEME;
   themeStatusBarItem.text = '$(color-mode)';
   themeStatusBarItem.tooltip = 'Toggle VS Code color mode';
@@ -138,6 +168,7 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(
     logger,
     switchThemeCommand,
+    configurationChanged,
     themeStatusBarItem
   );
 }
