539f2fd050b33917dba37a339a86f9b8f5685f63
Jakka Prihatna <jprihatna@gmail.com>
2022-12-08 19:45:38 +0700

    feat: replace extension logger with vscode LogOutputChannel
    Signed-off-by: Jakka Prihatna <jprihatna@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 import {
-  window, workspace, commands,
-  OutputChannel, ExtensionContext,
-  StatusBarAlignment, WorkspaceConfiguration, ConfigurationTarget
+  commands, ConfigurationTarget, ExtensionContext, LogOutputChannel, StatusBarAlignment,
+  window, workspace, WorkspaceConfiguration
 } from 'vscode';
 
 const SETTING_KEY_CONFIGURATION = 'darkLight';
@@ -14,50 +13,12 @@ const SETTING_KEY_HIGH_CONTRAST_DARK_THEME = 'preferredHighContrastColorTheme';
 
 const COMMAND_TOGGLE_THEME = 'darkLight.toggleColorMode';
 
-interface ILogger {
-  log(...msg: any[]): void;
-}
-
-class Logger {
-  readonly #output: OutputChannel;
-
-  constructor(output: OutputChannel) {
-    this.#output = output;
-  }
-
-  log(...messages: any[]): void {
-    const d = new Date();
-    const date = d.toISOString().split('T')[0];
-    const time = d.toTimeString().split(' ')[0];
-    const ms = (d.getMilliseconds() + '').padStart(3, '0');
-
-    const msg = messages.map((message: any) => {
-      if (typeof message === 'string') {
-        return message;
-      } else if (typeof message === 'object') {
-        return this.encode(message);
-      }
-      return message.toString();
-    }).join('\n');
-
-    this.#output.appendLine(`[${date} ${time}.${ms}] ${msg}`);
-  }
-
-  encode(obj: any): string {
-    return JSON.stringify(obj, null, 2);
-  }
-
-  dispose(): void {
-    this.#output.dispose();
-  }
-}
-
 class ColorMode {
-  #reversing: boolean = false;
+  #reverting: boolean = false;
 
-  readonly #logger?: ILogger;
+  readonly #logger: LogOutputChannel;
 
-  constructor(logger?: ILogger) {
+  constructor(logger: LogOutputChannel) {
     this.#logger = logger;
   }
 
@@ -69,8 +30,8 @@ class ColorMode {
     return workspace.getConfiguration(SETTING_KEY_WORKBENCH);
   }
 
-  get reversing(): boolean {
-    return this.#reversing;
+  get reverting(): boolean {
+    return this.#reverting;
   }
 
   get current(): string {
@@ -104,21 +65,21 @@ class ColorMode {
   }
 
   async revert(): Promise<void> {
-    if (this.#reversing === true) { return; }
+    if (this.#reverting === true) { return; }
 
-    this.#reversing = true;
+    this.#reverting = true;
     await this.toggle();
-    this.#reversing = false;
+    this.#reverting = false;
   }
 
   async toggle(): Promise<void> {
-    const newTheme = this.isDark ? this.light : this.dark;
-    this.#logger?.log(`${this.#reversing ? 'reversing' : 'switching'} color theme: ${this.current} ➝ ${newTheme}`);
+    const next = this.isDark ? this.light : this.dark;
+    this.#logger.info(`${this.#reverting ? 'reverting' : 'switching'} color theme: '${this.current}' ➝ '${next}'`);
 
     try {
-      await this.#workbench.update(SETTING_KEY_COLOR_THEME, newTheme, ConfigurationTarget.Global);
-    } catch (err) {
-      this.#logger?.log(err, this.#workbench);
+      await this.#workbench.update(SETTING_KEY_COLOR_THEME, next, ConfigurationTarget.Global);
+    } catch (err: any) {
+      this.#logger.error(err);
     }
   }
 
@@ -142,18 +103,19 @@ class ColorMode {
 }
 
 export function activate(context: ExtensionContext) {
-  const logger = new Logger(window.createOutputChannel('Dark Light'));
-  const colorMode = new ColorMode(logger);
+  const logger = window.createOutputChannel('Dark Light', { log: true });
 
-  logger.log('activating extension');
-  logger.log(`current color theme: ${colorMode.current}`);
+  const { publisher, name, version } = context.extension.packageJSON;
+  logger.info(`activating ${publisher}.${name} version ${version}`);
+
+  const colorMode = new ColorMode(logger);
+  logger.info(`current color theme: '${(colorMode.current)}'`);
 
   const switchThemeCommand = commands.registerCommand(COMMAND_TOGGLE_THEME, () => colorMode.toggle());
   const configurationChanged = workspace.onDidChangeConfiguration(async (e) => {
     if (e.affectsConfiguration(`${SETTING_KEY_WORKBENCH}.${SETTING_KEY_COLOR_THEME}`)) {
-      logger.log(`current color theme: ${colorMode.current}`);
-
-      if (!colorMode.reversing) {
+      logger.info(`current color theme: '${(colorMode.current)}'`);
+      if (!colorMode.reverting) {
         await colorMode.confirm();
       }
     }
